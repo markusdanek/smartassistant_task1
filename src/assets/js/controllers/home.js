fe5cfdbb47c606505f9ea4d6f66cf7d10f3aa730
Markus Danek <markus.danek@webducation.at>
2016-04-14 11:18:14 +0200

    added second step of glass-wizard '.facial-dependency'; switch between content wip

--- src/assets/js/controllers/home.js
+++ src/assets/js/controllers/home.js
@@ -2,8 +2,13 @@
 
   var app = angular.module('smartassistant');
 
-  app.controller('HomeCtrl', ['$scope', '$http', 'baseUrl', function($scope, $http, baseUrl) {
-
+  app.controller('HomeCtrl', ['$scope', function($scope) {
+    $('button#next').click(function(){
+      if( ('.facial-structure').is(':visible') ) {
+        ('.facial-structure').hide();
+        ('.facial-dependency').show();
+      }
+    });
   }]);
 
 })(window.angular);
