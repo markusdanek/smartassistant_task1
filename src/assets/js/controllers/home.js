3ecc6f826a8f3859505496a99445b07d526039f7
Markus Danek <markus.danek@webducation.at>
2016-04-14 11:27:03 +0200

    added basic div switch; see to-do info in templates/home.html

--- src/assets/js/controllers/home.js
+++ src/assets/js/controllers/home.js
@@ -3,10 +3,25 @@
   var app = angular.module('smartassistant');
 
   app.controller('HomeCtrl', ['$scope', function($scope) {
+    // TODO: use vanilla js instead of jquery; needs refactoring
     $('button#next').click(function(){
-      if( ('.facial-structure').is(':visible') ) {
-        ('.facial-structure').hide();
-        ('.facial-dependency').show();
+      if($('.facial-structure').is(':visible')) {
+        $('.facial-structure').hide();
+        $('.facial-dependency').show();
+        $('.navigation .next').hide();
+        $('.navigation .previous').show();
+        $('li.arrow:eq(0)').removeClass('active');
+        $('li.arrow:eq(1)').addClass('active');
+      }
+    });
+    $('button#previous').click(function(){
+      if($('.facial-dependency').is(':visible')) {
+        $('.facial-structure').show();
+        $('.facial-dependency').hide();
+        $('.navigation .next').show();
+        $('.navigation .previous').hide();
+        $('li.arrow:eq(0)').addClass('active');
+        $('li.arrow:eq(1)').removeClass('active');
       }
     });
   }]);
